8f179ab7971efab095d318e90897c82fd5957aa4
joshuaGodsaves <joshuaGodsaves19@gmail.com>
2019-08-25 01:00:19 +0100

    Added Nunjunks extends functionality Nothing much done

---
+++ README.md
@@ -0,0 +1,28 @@
+# Miousify-site Enterprise theme
+
+This theme is created with the needs of `big businesses` in mind. 
+It's design focuses on showing the most relevant information to the sites 
+user's, this is done with respect to what business primarily offers.
+
+## Core functionality if this theme.
+
+- Products page
+- Product page
+- Cart page
+- Checkout page
+- Accounts area (basically login, registeration and user account area).
+
+### Products page
+
+
+### Product page
+
+
+### Cart page
+
+
+### Checkout page
+
+
+### Account page
+
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-let express = require('express')
-let path = require('path')
-let app = express()
+let express = require('express');
+let path = require('path');
+let app = express();
 
-const views_url= path.join(__dirname, "src") 
+const views_url= path.join(__dirname, "src");
 
-let nunjucks = require('nunjucks')
+let nunjucks = require('nunjucks');
 
 nunjucks.configure(path.join(views_url), {
     autoescape: true,
@@ -12,23 +12,40 @@ nunjucks.configure(path.join(views_url), {
     watch: true
 });
 
-app.set("views", views_url)
+app.set("views", views_url);
 
 app.use("/resource/", express.static(path.join(__dirname, "src")))
 
 app.get("/", function (req, res) {
-    res.render("index.html", {list:[1,2,3,4]})
-})
+    res.render("index.html", {list:[1,2,3,4]});
+});
 
 app.get("/products", function (req, res) {
-    res.render("products.html", {list:[1,2,3,4]})
+    res.render("products.html", {list:[1,2,3,4]});
 })
 
 app.get("/products/:product", function (req, res) {
-    res.render("product.html", {list:[1,2,3,4,5,6]})
-})
+    res.render("product.html", {list:[1,2,3,4,5,6]});
+});
 
+app.get("/checkout", function (req, res) {
+    res.render("checkout.html", {list:[1,2,3,4,5,6]});
+});
+
+app.get("/signup", function (req, res) {
+    res.render("signup.html", {list:[1,2,3,4,5,6]});
+});
+
+app.get("/signin", function (req, res) {
+    res.render("signin.html", {list:[1,2,3,4,5,6]});
+});
+
+
+
+app.get("/cart", function (req, res) {
+    res.render("cart.html", {list:[1,2,3,4,5,6]});
+});
 
 app.listen(4100, function () {
-    console.log("is listening")
-})
+    console.log("is listening");
+});
